d7c7cb49dec306cfbe95c68ed4302d200da8e3e3
Guilherme Cruz <guilhermecruz@gmail.com>
2016-08-09 10:40:37 -0300

    Update notify.sh

--- notify.sh
+++ notify.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# Update the cron's limited enviroment variables
+PATH=$PATH:/usr/local/bin
+
 osascript -e 'display notification "'"$1"'"  sound name "Ping.aiff" with title "Excuse me" '
